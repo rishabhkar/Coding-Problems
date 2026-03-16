a22a068d999995b9fa11d308ea30f647ead9039b
Rishabh Kar <rk.official22@gmail.com>
2026-03-16 21:37:43 +0530

    Arrays and Strings

--- leetcode/easy/linked_list/21_merge_two_sorted_lists/MergeTwoSortedLists.java
+++ leetcode/easy/linked_list/21_merge_two_sorted_lists/MergeTwoSortedLists.java
@@ -1,23 +1,70 @@
 public class MergeTwoSortedLists {
-    static class ListNode {
-        int val;
-        ListNode next;
+  static class ListNode {
+    int val;
+    ListNode next;
 
-        ListNode() {
-        }
-
-        ListNode(int val) {
-            this.val = val;
-        }
-
-        ListNode(int val, ListNode next) {
-            this.val = val;
-            this.next = next;
-        }
+    ListNode() {
     }
 
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        // TODO: Implement this method.
-        return null;
+    ListNode(int val) {
+      this.val = val;
     }
+
+    ListNode(int val, ListNode next) {
+      this.val = val;
+      this.next = next;
+    }
+  }
+
+  public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+    // Case 1: If List 1 and List 2 both are null -> Return new linked list
+    if (list1 == null && list2 == null) return null;
+
+    // Case 2: If List 1 is not null and List 2 is null -> Return List 1
+    if (list1 != null && list2 == null) return list1;
+
+    // Case 3: If List 1 is null and List 2 is not null -> Return List 2
+    if (list1 == null && list2 != null) return list2;
+
+    // Case 4: Usual case mentioned in the question
+    ListNode pointer1 = list1;
+    ListNode pointer2 = list2;
+    ListNode newHead = new ListNode();
+    ListNode previousNode = newHead;
+
+    while (pointer1 != null && pointer2 != null) {
+
+      if (pointer1.val < pointer2.val) {
+
+        ListNode newNode = new ListNode(pointer1.val);
+        previousNode.next = newNode;
+        previousNode = newNode;
+        pointer1 = pointer1.next;
+
+      } else if (pointer1.val >= pointer2.val) {
+
+        ListNode newNode = new ListNode(pointer2.val);
+        previousNode.next = newNode;
+        previousNode = newNode;
+        pointer2 = pointer2.next;
+      }
+    }
+
+    while (pointer1 != null) {
+      ListNode newNode = new ListNode(pointer1.val);
+      previousNode.next = newNode;
+      previousNode = newNode;
+      pointer1 = pointer1.next;
+    }
+
+    while (pointer2 != null) {
+      ListNode newNode = new ListNode(pointer2.val);
+      previousNode.next = newNode;
+      previousNode = newNode;
+      pointer2 = pointer2.next;
+    }
+
+    return newHead.next;
+  }
 }
--- leetcode/medium/binary_search/81_search_in_rotated_sorted_array_ii/SearchInRotatedSortedArrayII.java
+++ leetcode/medium/binary_search/81_search_in_rotated_sorted_array_ii/SearchInRotatedSortedArrayII.java
@@ -1,6 +1,42 @@
 public class SearchInRotatedSortedArrayII {
-    public boolean search(int[] nums, int target) {
-        // TODO: Implement this method.
-        return false;
+  public boolean search(int[] nums, int target) {
+
+    // Case 1: If array is null (bypassing constraints) -> Return false
+    if (nums == null || nums.length == 0) return false;
+
+    // Case 2: Single element array and target matching -> Return true
+    if (nums.length == 1 && nums[0] == target) return true;
+
+    // Case 3: Single element array and target not matching -> Return false
+    if (nums.length == 1 && nums[0] != target) return false;
+
+    // Case 4: Usual case
+    // Identify the pivot
+    int pivotIndex = 0;
+    while (pivotIndex + 1 < nums.length) {
+      if (nums[pivotIndex + 1] < nums[pivotIndex]) {
+        break;
+      } else pivotIndex++;
     }
+
+    return binarySearch(nums, target, 0, pivotIndex)
+        || binarySearch(nums, target, pivotIndex + 1, nums.length - 1);
+  }
+
+  public boolean binarySearch(int[] nums, int target, int left, int right) {
+
+    while (left <= right) {
+      int mid = (left + right) / 2;
+
+      if (target == nums[mid]) {
+        return true;
+      } else if (target > nums[mid]) {
+        left = mid + 1;
+      } else if (target < nums[mid]) {
+        right = mid - 1;
+      }
+    }
+
+    return false;
+  }
 }
