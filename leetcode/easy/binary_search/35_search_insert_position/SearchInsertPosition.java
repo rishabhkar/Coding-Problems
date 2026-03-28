089067e842e8e4856b32a51e8bf47c3012a388a5
Rishabh Kar <rk.official22@gmail.com>
2026-03-28 10:39:46 +0530

    Adding Valid Parenthesis Code

--- leetcode/easy/binary_search/35_search_insert_position/SearchInsertPosition.java
+++ leetcode/easy/binary_search/35_search_insert_position/SearchInsertPosition.java
@@ -1,6 +1,32 @@
+import java.util.*;
+
 public class SearchInsertPosition {
     public int searchInsert(int[] nums, int target) {
-        // TODO: Implement this method.
-        return 0;
+
+        // Case 1: If array passed is null or empty (bypassing constraints) -> Throw Illegal Argument
+      // Exception
+        if (nums.length == 0 || nums == null) throw new IllegalArgumentException("Array passed is empty");
+
+      // Case 2: Usual case
+      int indexFound = Integer.MIN_VALUE;
+      int[] temporaryNums = nums;
+      int left = 0;
+      int right = temporaryNums.length-1;
+
+      while (left < right) {
+
+        int mid = (left + right) / 2;
+
+        if (mid == target) indexFound = mid;
+        else if (target < mid) {
+          right = mid - 1;
+//          temporaryNums = Arrays.copyOf(nums, )
+          // Currently on hold
+        }
+
+      }
+
+      return indexFound;
     }
+
 }
--- leetcode/easy/stack/20_valid_parentheses/ValidParentheses.java
+++ leetcode/easy/stack/20_valid_parentheses/ValidParentheses.java
@@ -1,6 +1,27 @@
+import java.util.*;
+
 public class ValidParentheses {
     public boolean isValid(String s) {
-        // TODO: Implement this method.
-        return false;
+
+      Stack<Character> stack = new Stack();
+
+      for (int i=0; i<s.length(); i++) {
+
+        if (s.charAt(i) == '(' || s.charAt(i) == '['
+            || s.charAt(i) == '{') {
+          stack.push(s.charAt(i));
+        }
+        else {
+          if (!stack.isEmpty()) {
+            if(s.charAt(i) == ')' && stack.peek() == '(') stack.pop();
+            else if(s.charAt(i) == '}' && stack.peek() == '{') stack.pop();
+            else if(s.charAt(i) == ']' && stack.peek() == '[') stack.pop();
+            else stack.push(s.charAt(i));
+          } else stack.push(s.charAt(i));
+        }
+
+      }
+
+      return stack.isEmpty();
     }
 }
